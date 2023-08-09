9f90c27964e2d1faa11d45ab51f927638650483e
“fredrickUdom” <fredrickudom@gmail.com>
2023-08-09 21:18:12 +0100

    hot fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './common/user/user.module';
 
+const dbUrl = process.env.DB_URI || 'mongodb+srv://e_commerce:12hdb6e9wdRSwGqw@cluster0.ubyns6e.mongodb.net/';
 @Module({
   imports: [
     ConfigModule.forRoot({envFilePath: '.env',
     isGlobal: true
   }),
-    MongooseModule.forRoot(process.env.DB_URI),
+    MongooseModule.forRoot(dbUrl),
     UserModule
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  const serverPort = await app.listen(process.env.SERVER_PORT);
+  await app.listen(port);
   // console.log(serverPort)
 }
 bootstrap();
